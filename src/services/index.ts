eb5a608139bbd642959133437723ec5407ed9ffb
oopaze <pedroosd28@gmail.com>
2022-10-19 03:23:11 -0300

    feat: pointing api to staging

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,7 @@ import { IClient } from "../utils/api/@types/client";
 // import AxiosClient from "../utils/api/impl/axios";
 import FetchClient from "../utils/api/impl/fetch";
 
-const getClient = (): IClient => new FetchClient("http://192.168.100.66:5000")
+const getClient = (): IClient => new FetchClient("https://expensies-api.herokuapp.com")
 
 const api = getClient()
 
